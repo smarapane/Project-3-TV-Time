e2aed18e97ca7c41f9e228ba77b1745569a9780d
smarapane <sidathmarapane@yahoo.com>
2023-04-23 23:21:31 -0400

    Revert "Add basic tooltip"
    This reverts commit 295a67b9da8b08689c88a729688f5756cf6f266e.

--- js/charBarChart.js
+++ js/charBarChart.js
@@ -132,15 +132,15 @@ class CharBarChart {
         group,
         (v) => v.length,
         (d) => d.episode
+        //d => d.season
       );
       return { season, countByEpisode };
     });
 
 const episodeCountsArray = episodeCounts.map(d => {
   const season = d.season;
-  const episodeArray = Array.from(d.countByEpisode.keys());
   const countsArray = Array.from(d.countByEpisode.values());
-  return { season, episodeArray, ...countsArray };
+  return { season, ...countsArray };
 });
 
 const stack = d3.stack().keys(Array.from({length: 26}, (_, i) => String(i + 1)));
@@ -221,8 +221,7 @@ vis.stackedData = stack(episodeCountsArray);
     .on('mouseover', (event,d) => {
       d3.select('#tooltip')
         .style('opacity', 1)
-        .html(`<div class="tooltip-label">Season ${d.data.season} Episode ${d.data.episode}</div>`);
-      console.log(d);
+        .html(`<div class="tooltip-label">Test</div>`);
     })
     .on('mousemove', (event) => {
       d3.select('#tooltip')
